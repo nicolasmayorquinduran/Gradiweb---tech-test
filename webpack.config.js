2daa8b870ac0f48c14f87e9deb1499a4d3dd9d1b
Nicolas Mayorquin Duran <nicolasmayorquinduran@Nicolass-MacBook-Air.local>
2025-09-05 13:50:18 -0500

    feat(webpack): setting out loaders and plugins for assets and preproccesor css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: './src/app.js',
@@ -10,9 +13,40 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        type: "asset",
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource', // Maneja fuentes
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
       },
     ],
   },
-  mode: 'development',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+devServer: {
+  static: {
+    directory: path.join(__dirname, 'public'),
+  },
+  hot: true,
+  port: 3000,
+  open: true,
+  devMiddleware: {
+    writeToDisk: true,
+  },
+},
+  mode,
 };
